b04c78d48a9d17624d165d90f778e7625375aed4
bumsyalao <alaobunmi93@gmail.com>
2018-06-10 21:57:18 +0100

    complete version1.0

--- script.js
+++ script.js
@@ -1,57 +1,89 @@
 
-const myName ="Bunmi";
-const yourCrush ="";
+document.querySelector('#showMeButton').addEventListener('click', function() {
+    //Get your name, your crush and calculate crush
+    var yourName = document.querySelector('#yourNameInput').value;
+    var yourCrush = document.querySelector('#yourCrushInput').value;
 
-// set names to lowercase
-
-const myNameList = myName.toLowerCase().split('');
-const yourCrushList = yourCrush.toLowerCase().split('');
-
-// remove duplicates in both array.
-
-myNameList.forEach((letter, index) => {
-  const position = yourCrushList.indexOf(letter);   
+    var result = computeResult(yourName, yourCrush);
+    document.querySelector('#display-here').innerHTML = result;
+    
+    // show result page and remove form page.
+    var resultDiv = document.getElementById("result");
+      resultDiv.classList.remove("hide");
+    var formDiv = document.getElementById("form");
+      formDiv.classList.add("hide");
   
-  if(position > -1){
-    yourCrushList.splice(position, 1);
-    myNameList.splice(index, 1);
+   
+  });
+  //hide result page, go to form page
+  document.querySelector('#gobackButton').addEventListener('click', function() {
+    var resultDiv = document.getElementById("result");
+      resultDiv.classList.add("hide");
+    var formDiv = document.getElementById("form");
+      formDiv.classList.remove("hide");
+   
+  });
+  
+  //function to compute result.
+  
+  function computeResult(yourName, yourCrush) {
+  
+  
+  // set names to lowercase
+  
+  const myNameList = yourName.toLowerCase().split('');
+  const yourCrushList = yourCrush.toLowerCase().split('');
+  
+  // remove duplicates in both array.
+  
+  myNameList.forEach((letter, index) => {
+    const position = yourCrushList.indexOf(letter);   
+    
+    if(position > -1){
+      yourCrushList.splice(position, 1);
+      myNameList.splice(index, 1);
+    }
+  
+  });
+  
+  //get flame position and flame
+  const flamePosition = myNameList.length + yourCrushList.length;
+  
+  let flame = flamePosition;
+  
+  if (flame > 6){
+    flame = flame % 6; 
   }
-
-});
-
-//get flame position and flame
-const flamePosition = myNameList.length + yourCrushList.length;
-
-let flame = flamePosition;
-
-if (flame > 6){
-  flame = flame % 6; 
-}
-
-
-//function to check flames digit
-const checkFlames = (num) => {
-
-  switch(num){
-    case 1:
-      console.log("Y'all will just be Friends");
-      break;
-    case 2:
-      console.log("Aww, you are lovers");
-      break;
-    case 3:
-      console.log("You're Admirers");
-      break;
-    case 4:
-      console.log("You'll get married to " + yourCrush);
-      break;
-    case 5:
-      console.log("You will be enemies");
-      break;
-    case 6:
-      console.log(yourCrush +" is your secret admirer");
-      break;
+  
+  
+  //function to check flames digit
+  const checkFlames = (num) => {
+    let prediction = '';
+    switch(parseInt(num)){
+      case 1:
+        prediction = "Y'all will just be Friends";
+        break;
+      case 2:
+        prediction= "Aww, you are lovers";
+        break;
+      case 3:
+        prediction= "You're Admirers";
+        break;
+      case 4:
+        prediction= "You'll get married to " + yourCrush;
+        break;
+      case 5:
+        prediction= "You will be enemies";
+        break;
+      case 6:
+        prediction= yourCrush +" is your secret admirer";
+        break;
+    }
+    return prediction;
   }
-}
-
-checkFlames(flame);
+  
+   return checkFlames(flame);
+  }
+  
+  
+  
