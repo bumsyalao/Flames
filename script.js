db9db32fca712da991fa9267493ccb4e8d574cd0
bumsyalao <alaobunmi93@gmail.com>
2018-06-11 00:21:28 +0100

    --amend

--- script.js
+++ script.js
@@ -78,8 +78,6 @@ document.querySelector('#showMeButton').addEventListener('click', function() {
       case 6:
         prediction= yourCrush +" is your secret admirer";
         break;
-        default:
-          prediction = "You have not entered any name!";
     }
     return prediction;
   }
