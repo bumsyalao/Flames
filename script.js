56379bc86205311548ae0f900fedfe0a53ba7b03
bumsyalao <alaobunmi93@gmail.com>
2018-06-11 17:52:05 +0100

    Add delay

--- script.js
+++ script.js
@@ -1,91 +1,91 @@
 
 document.querySelector('#showMeButton').addEventListener('click', function() {
-    //Get your name, your crush and calculate crush
-    var yourName = document.querySelector('#yourNameInput').value;
-    var yourCrush = document.querySelector('#yourCrushInput').value;
-
-    var result = computeResult(yourName, yourCrush);
+  var yourName = document.querySelector('#yourNameInput').value;
+  var yourCrush = document.querySelector('#yourCrushInput').value;
+  
+  if(yourName.length > 0 && yourCrush.length > 0) {
+  
+  var resultDiv = document.getElementById("result");
+    resultDiv.classList.remove("hide");
+  var formDiv = document.getElementById("form");
+    formDiv.classList.add("hide");
+  setTimeout(function(){
+      var result = computeResult(yourName, yourCrush);
+// console.log(result);
     document.querySelector('#display-here').innerHTML = result;
-    
-    // show result page and remove form page.
-    var resultDiv = document.getElementById("result");
-      resultDiv.classList.remove("hide");
-    var formDiv = document.getElementById("form");
-      formDiv.classList.add("hide");
-  
-   
-  });
-  //hide result page, go to form page
-  document.querySelector('#gobackButton').addEventListener('click', function() {
-    var resultDiv = document.getElementById("result");
-      resultDiv.classList.add("hide");
-    var formDiv = document.getElementById("form");
-      formDiv.classList.remove("hide");
-   
-  });
-  
-  //function to compute result.
-  
-  function computeResult(yourName, yourCrush) {
-  
-  
-  // set names to lowercase
-  
-  const myNameList = yourName.toLowerCase().split('');
-  const yourCrushList = yourCrush.toLowerCase().split('');
-  
-  // remove duplicates in both array.
-  
-  myNameList.forEach((letter, index) => {
-    const position = yourCrushList.indexOf(letter);   
-    
-    if(position > -1){
-      yourCrushList.splice(position, 1);
-      myNameList.splice(index, 1);
-    }
-  
-  });
-  
-  //get flame position and flame
-  const flamePosition = myNameList.length + yourCrushList.length;
-  
-  let flame = flamePosition;
-  
-  if (flame > 6){
-    flame = flame % 6; 
+    }, 3000);
+
   }
+});
+
+document.querySelector('#gobackButton').addEventListener('click', function() {
+  var resultDiv = document.getElementById("result");
+    resultDiv.classList.add("hide");
+  var formDiv = document.getElementById("form");
+    formDiv.classList.remove("hide");
+ 
+});
+
+
+function computeResult(yourName, yourCrush) {
+
+
+// set names to lowercase
+
+const myNameList = yourName.toLowerCase().split('');
+const yourCrushList = yourCrush.toLowerCase().split('');
+
+// remove duplicates in both array.
+
+myNameList.forEach((letter, index) => {
+  const position = yourCrushList.indexOf(letter);   
   
-  
-  //function to check flames digit
-  const checkFlames = (num) => {
-    let prediction = '';
-    switch(parseInt(num)){
-      case 1:
-        prediction = "Y'all will just be Friends";
-        break;
-      case 2:
-        prediction= "Aww, you are lovers";
-        break;
-      case 3:
-        prediction= "You're Admirers";
-        break;
-      case 4:
-        prediction= "You'll get married to " + yourCrush;
-        break;
-      case 5:
-        prediction= "You will be enemies";
-        break;
-      case 6 || 0:
-        prediction= yourCrush +" is your secret admirer";
-        break;
-      default:
-        prediction= "You have not entered any name";
-    }
-    return prediction;
+  if(position > -1){
+    yourCrushList.splice(position, 1);
+    myNameList.splice(index, 1);
   }
-  
-   return checkFlames(flame);
+
+});
+
+//get flame position and flame
+const flamePosition = myNameList.length + yourCrushList.length;
+
+let flame = flamePosition;
+
+if (flame > 6){
+  flame = flame % 6; 
+}
+
+
+//function to check flames digit
+const checkFlames = (num) => {
+  let prediction = '';
+  switch(parseInt(num)){
+    case 1:
+      prediction = "You and "+yourCrush+" will just be friends.";
+      break;
+    case 2:
+      prediction= "Aww, You and "+yourCrush+" are Lovers";
+      break;
+    case 3:
+      prediction= "you and "+yourCrush+" are Admirers";
+      break;
+    case 4:
+      prediction= "You'll get married to " + yourCrush;
+      break;
+    case 5:
+      prediction= "You will be enemies!";
+      break;
+    case 6:
+      prediction= yourCrush +" is your secret admirer";
+      break;
+    default:
+      prediction = "You have not entered any name!";
   }
-  
-  
-  
+  return prediction;
+}
+
+ return checkFlames(flame);
+}
+
+
